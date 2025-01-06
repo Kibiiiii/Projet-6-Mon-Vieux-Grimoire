bbe17594e3e93be00f637b9343dfc38c3d192284
Neary <kibishiigaming@gmail.com>
2025-01-06 11:06:23 +0100

    Creation du middleware et auth.js.

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const auth = require('auth');
 const router = express.Router();
 const Thing = require('../models/Thing');
 
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', stuffCtrl.createThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
-router.get('/:id', stuffCtrl.getOneThing);
-router.get('/', stuffCtrl.getAllStuff);
+router.post('/', auth, stuffCtrl.createThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.get('/', auth, stuffCtrl.getAllStuff);
 
 module.exports = router;
